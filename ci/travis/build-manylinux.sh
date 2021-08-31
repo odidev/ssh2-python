5fe8c876b39c8c417d53b9721f2b540dff638e9f
odidev <odidev@puresoftware.com>
2021-08-31 13:38:15 +0530

    Add Linux AArch64 wheel build support

--- ci/travis/build-manylinux.sh
+++ ci/travis/build-manylinux.sh
@@ -5,6 +5,10 @@ docker_tag="parallelssh/ssh2-manylinux"
 rm -rf build ssh2/libssh2.*
 python ci/appveyor/fix_version.py .
 
+if [[ `uname -m` == "aarch64" ]]; then
+  sed -i 's/2010_x86_64/2014_aarch64/g' ci/docker/manylinux/Dockerfile
+fi
+
 docker pull $docker_tag || echo
 docker build --cache-from $docker_tag ci/docker/manylinux -t $docker_tag
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then docker push $docker_tag; fi
